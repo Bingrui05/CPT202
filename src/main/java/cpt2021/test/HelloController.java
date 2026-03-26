7ca32387e7b6110e84d9b6d109c0a5333e280ec5
Bingrui05 <3480894505@qq.com>
2026-03-26 23:43:51 +0800

    first commit

---
+++ src/main/java/cpt2021/test/HelloController.java
@@ -0,0 +1,18 @@
+package cpt2021.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot is running successfully!";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Test API works!";
+    }
+}
